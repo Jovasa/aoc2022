a6fec135a1f499700dd7c1079d1ce3eb97c474ee
Joose Sainio <joose.sainio@tuni.fi>
2022-12-01 07:58:33 +0200

    Day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let reader = BufReader::new(File::open("data/day1.txt").unwrap());
+
+    let mut data = Vec::new();
+    let mut total = 0;
+    for line in reader.lines() {
+        let line = line.unwrap();
+        match line.parse::<u32>() {
+            Ok(t) => total += t,
+            Err(_) => {
+                data.push(total);
+                total = 0;
+            }
+        }
+    }
+    data.push(total);
+    data.sort();
+    println!("{}", data.iter().rev().next().unwrap());
+    println!("{}", data.iter().rev().take(3).fold(0, |x, o| x + o))
 }
